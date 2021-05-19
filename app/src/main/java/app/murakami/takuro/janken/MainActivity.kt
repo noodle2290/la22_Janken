fe798c0c625735605dada309bb8757a2b195e305
takuro <67560179+nuaseTakuro@users.noreply.github.com>
2021-05-20 03:53:37 +0900

    ジャンケンアプリ画像を追加して勝敗結果の統計を追加

--- app/src/main/java/app/murakami/takuro/janken/MainActivity.kt
+++ app/src/main/java/app/murakami/takuro/janken/MainActivity.kt
@@ -11,27 +11,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var win = 0
+        var draw = 0
+        var lose = 0
+
         gooButton.setOnClickListener {
             player.text = "あなたの手はグーです"
             val number: Int = Random.nextInt(3)
 
             when(number){
                 0 -> {
-                    cpu.text = "相手の手はグーです"
+                    cpu.setImageResource(R.drawable.goo)
                     result.text = "引き分けです"
                     result.setTextColor(Color.parseColor("#8a000000"))
+                    draw += 1
                 }
                 1 -> {
-                    cpu.text = "相手の手はチョキです"
+                    cpu.setImageResource(R.drawable.choki)
                     result.text = "あなたの勝ちです"
                     result.setTextColor(Color.parseColor("#f44336"))
+                    win += 1
                 }
                 2 -> {
-                    cpu.text = "相手の手はパーです"
+                    cpu.setImageResource(R.drawable.paa)
                     result.text = "あなたの負けです"
                     result.setTextColor(Color.parseColor("#2196f3"))
+                    lose += 1
                 }
             }
+
+            all.text = win.toString() + "勝" + draw.toString() + "分" + lose.toString() + "負"
         }
 
         chokiButton.setOnClickListener {
@@ -40,21 +49,25 @@ class MainActivity : AppCompatActivity() {
 
             when(number){
                 0 -> {
-                    cpu.text = "相手の手はチョキです"
+                    cpu.setImageResource(R.drawable.choki)
                     result.text = "引き分けです"
                     result.setTextColor(Color.parseColor("#8a000000"))
+                    draw += 1
                 }
                 1 -> {
-                    cpu.text = "相手の手はパーです"
+                    cpu.setImageResource(R.drawable.paa)
                     result.text = "あなたの勝ちです"
                     result.setTextColor(Color.parseColor("#f44336"))
+                    win += 1
                 }
                 2 -> {
-                    cpu.text = "相手の手はグーです"
+                    cpu.setImageResource(R.drawable.goo)
                     result.text = "あなたの負けです"
                     result.setTextColor(Color.parseColor("#2196f3"))
+                    lose += 1
                 }
             }
+            all.text = win.toString() + "勝" + draw.toString() + "分" + lose.toString() + "負"
         }
 
         paaButton.setOnClickListener {
@@ -63,21 +76,25 @@ class MainActivity : AppCompatActivity() {
 
             when(number){
                 0 -> {
-                    cpu.text = "相手の手はパーです"
+                    cpu.setImageResource(R.drawable.paa)
                     result.text = "引き分けです"
                     result.setTextColor(Color.parseColor("#8a000000"))
+                    draw += 1
                 }
                 1 -> {
-                    cpu.text = "相手の手はグーです"
+                    cpu.setImageResource(R.drawable.goo)
                     result.text = "あなたの勝ちです"
                     result.setTextColor(Color.parseColor("#f44336"))
+                    win += 1
                 }
                 2 -> {
-                    cpu.text = "相手の手はチョキです"
+                    cpu.setImageResource(R.drawable.choki)
                     result.text = "あなたの負けです"
                     result.setTextColor(Color.parseColor("#2196f3"))
+                    lose += 1
                 }
             }
+            all.text = win.toString() + "勝" + draw.toString() + "分" + lose.toString() + "負"
         }
     }
 }
